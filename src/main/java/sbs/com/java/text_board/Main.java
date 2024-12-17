44f54f11ec3e5ece3bbf7b406cad2397fa26453d
020226 <mmin08@naver.com>
2024-12-17 13:59:16 +0900

    article 클래스 도입

--- src/main/java/sbs/com/java/text_board/Main.java
+++ src/main/java/sbs/com/java/text_board/Main.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
 public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
+    int lastArticleId = 0;
     System.out.println("== 자바 텍스트 게시판 시작 ==");
     while(true) {
       System.out.println("명령) ");
@@ -17,9 +18,17 @@ public class Main {
         System.out.println("내용 : ");
         String content = sc.nextLine();
 
-        int id = 1;
+        int id = ++lastArticleId;
 
-        System.out.printf("%d번 게시물이 등록되었습니다.\n", id);
+        // 객체는 항상 새롭게 만들어진다
+        Article article = new Article(); // 게시물 작성 후 article 객체 생성
+        // 입력 받은 내용을 객체와 연결
+        article.id = id;
+        article.subject = subject;
+        article.content = content;
+
+        System.out.println("생성된 게시물 객체" + article);
+        System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       }
 
       else if(cmd.equals("exit")) {
@@ -33,4 +42,10 @@ public class Main {
      System.out.println("== 자바 텍스트 게시판 종료 ==");
      sc.close();
   }
+}
+
+class Article {
+  int id;
+  String subject;
+  String content;
 }
