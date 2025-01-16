2035b928161262445126506affecb3a16c99322e
020226 <mmin08@naver.com>
2025-01-16 18:50:49 +0900

    C:/Program Files/Git/usr/article/detail 입력시 가장 마지막 입력된 게시물 상세보기

--- src/main/java/sbs/com/java/text_board/Main.java
+++ src/main/java/sbs/com/java/text_board/Main.java
@@ -6,6 +6,8 @@ public class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     int lastArticleId = 0;
+    Article lastArticle = null;
+
     System.out.println("== 자바 텍스트 게시판 시작 ==");
     while(true) {
       System.out.println("명령) ");
@@ -21,12 +23,23 @@ public class Main {
         int id = ++lastArticleId;
 
         Article article = new Article(id, subject, content); // 게시물 작성 후 article 객체 생성
+        lastArticle = article;
 
         System.out.println("생성된 게시물 객체: " + article.toString());
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
       }
 
-      else if(cmd.equals("exit")) {
+      else if (cmd.equals("/usr/article/detail")) {
+        Article article = lastArticle;
+        if(article == null) {
+          System.out.println("게시물이 존재하지 않습니다.");
+          continue;
+        }
+        System.out.println("== 게시물 상세보기 ==");
+        System.out.printf("번호 : %d\n", article.id);
+        System.out.printf("제목 : %s\n", article.subject);
+        System.out.printf("내용 : %s\n", article.content);
+      } else if (cmd.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
         break; // 반복문 빠져나옴
       }
