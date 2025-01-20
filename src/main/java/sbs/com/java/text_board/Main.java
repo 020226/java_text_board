ecabf37ae078fd7d382181e0c3834eec252c1b6b
020226 <mmin08@naver.com>
2025-01-20 10:48:56 +0900

    rq도입

--- src/main/java/sbs/com/java/text_board/Main.java
+++ src/main/java/sbs/com/java/text_board/Main.java
@@ -1,66 +1,69 @@
 package sbs.com.java.text_board;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-  static void makeTestData(List<Article> articles) { // 테스트 게시물 생성
+  static void makeTestData(List<Article> articles) {
     articles.add(new Article(1, "제목1", "내용1"));
     articles.add(new Article(2, "제목2", "내용2"));
     articles.add(new Article(3, "제목3", "내용3"));
   }
+
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
     List<Article> articles = new ArrayList<>();
 
-    makeTestData(articles); // 테스트 게시물
+    makeTestData(articles);
 
-    int lastArticleId = articles.get(articles.size() -1).id;
+    int lastArticleId = articles.get(articles.size() - 1).id;
 
     System.out.println("== 자바 텍스트 게시판 시작 ==");
-    while(true) {
-      System.out.println("명령) ");
+
+    while (true) {
+      System.out.print("명령) ");
       String cmd = sc.nextLine();
 
-      if(cmd.equals("/usr/article/write")) {
+      Rq rq = new Rq(cmd);
+
+      if (rq.urlPath.equals("/usr/article/write")) {
         System.out.println("== 게시물 작성 ==");
-        System.out.println("제목 : ");
+        System.out.print("제목 : ");
         String subject = sc.nextLine();
-        System.out.println("내용 : ");
+
+        System.out.print("내용 : ");
         String content = sc.nextLine();
 
         int id = ++lastArticleId;
 
-        Article article = new Article(id, subject, content); // 게시물 작성 후 article 객체 생성
+        Article article = new Article(id, subject, content);
 
         articles.add(article);
+
         System.out.printf("%d번 게시물이 등록되었습니다.\n", article.id);
-
-
       } else if (cmd.equals("/usr/article/list")) {
-          if(articles.isEmpty()) {
-            System.out.println("현재 게시물이 존재하지 않습니다.");
-            continue;
-          }
-
-          System.out.println("== 게시물 리스트 ==");
-
-          System.out.println("번호 | 제목");
-          for (int i = articles.size() - 1; i >= 0; i--) { // articles.size() - 1: 인덱스는 0부터 시작
-            Article article = articles.get(i);
-            System.out.printf("%d | %s\n", article.id, article.subject);
-          }
-
-      } else if (cmd.equals("/usr/article/detail")) {
         if(articles.isEmpty()) {
-          System.out.println("게시물이 존재하지 않습니다.");
+          System.out.println("현재 게시물이 존재하지 않습니다.");
           continue;
         }
 
-        Article article =  articles.get(articles.size() -1);
+        System.out.println("== 게시물 리스트 ==");
 
-        if(article == null) {
+        System.out.println("번호 | 제목");
+
+        for(int i = articles.size() - 1; i >= 0; i--) {
+          Article article = articles.get(i);
+          System.out.printf("%d | %s\n", article.id, article.subject);
+        }
+
+      } else if (rq.urlPath.equals("/usr/article/detail")) {
+        if(articles.isEmpty()) {
+          System.out.println("현재 게시물이 존재하지 않습니다.");
+          continue;
+        }
+
+        Article article = articles.get(articles.size() - 1);
+
+        if (article == null) {
           System.out.println("해당 게시물이 존재하지 않습니다.");
           continue;
         }
@@ -69,16 +72,16 @@ public class Main {
         System.out.printf("번호 : %d\n", article.id);
         System.out.printf("제목 : %s\n", article.subject);
         System.out.printf("내용 : %s\n", article.content);
-      } else if (cmd.equals("exit")) {
+      } else if (rq.urlPath.equals("exit")) {
         System.out.println("프로그램을 종료합니다.");
-        break; // 반복문 빠져나옴
-      }
-      else {
-        System.out.println("잘못된 명령어입니다.");
+        break;
+      } else {
+        System.out.println("잘못 된 명령어입니다.");
       }
     }
-     System.out.println("== 자바 텍스트 게시판 종료 ==");
-     sc.close();
+
+    System.out.println("== 자바 텍스트 게시판 종료 ==");
+    sc.close();
   }
 }
 
@@ -87,7 +90,7 @@ class Article {
   String subject;
   String content;
 
-  public Article(int id, String subject, String content) {
+  Article(int id, String subject, String content) {
     this.id = id;
     this.subject = subject;
     this.content = content;
@@ -96,5 +99,54 @@ class Article {
   @Override
   public String toString() {
     return "{id: %d, subject: \"%s\", content: \"%s\"}".formatted(id, subject, content);
+  }
+}
+
+class Rq {
+  String url;
+  Map<String, String> params;
+  String urlPath;
+
+  Rq(String url) {
+    this.url = url;
+    params = Util.getParamsFromUrl(this.url);
+    urlPath = Util.getPathFromUrl(this.url);
+  }
+
+  public Map<String, String> getParams() {
+    return params;
+  }
+
+  public String getUrlPath() {
+    return urlPath;
+  }
+}
+
+class Util {
+  static Map<String, String> getParamsFromUrl(String url) {
+    Map<String, String> params = new HashMap<>();
+    String[] urlBits = url.split("\\?", 2);
+
+    if(urlBits.length == 1) {
+      return params;
+    }
+
+    String queryStr = urlBits[1];
+
+    for(String bit : queryStr.split("&")) {
+      String[] bits = bit.split("=", 2);
+
+      if(bits.length == 1) {
+        continue;
+      }
+
+      params.put(bits[0], bits[1]);
+    }
+
+    return params;
+  }
+
+  static String getPathFromUrl(String url) {
+    return url.split("\\?", 2)[0];
   }
 }
