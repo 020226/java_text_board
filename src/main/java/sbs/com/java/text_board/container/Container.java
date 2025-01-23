32d3f46b8a07f0884f1bc61f619e6574878ecc7b
020226 <mmin08@naver.com>
2025-01-23 20:15:59 +0900

    ArticleService와 ArticleRepository 도입

--- src/main/java/sbs/com/java/text_board/container/Container.java
+++ src/main/java/sbs/com/java/text_board/container/Container.java
@@ -1,14 +1,20 @@
 package sbs.com.java.text_board.container;
 
 import sbs.com.java.text_board.article.ArticleController;
+import sbs.com.java.text_board.article.ArticleRepository;
+import sbs.com.java.text_board.article.ArticleService;
 
 import java.util.Scanner;
 public class Container {
   public static Scanner sc;
+  public static ArticleRepository articleRepository;
+  public static ArticleService articleService;
   public static ArticleController articleController;
 
   static {
     sc = new Scanner(System.in);
+    articleRepository = new ArticleRepository();
+    articleService = new ArticleService();
     articleController = new ArticleController();
   }
 }
